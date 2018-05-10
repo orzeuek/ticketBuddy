711f80df8099848ca3d6c365f63ab131a8528e40
Rafał Orłowski <orlowski@ifelse.pl>
2018-05-10 20:49:12 +0200

    stations classifier extended WIP

--- src/stations_extraction.py
+++ src/stations_extraction.py
@@ -1,12 +1,69 @@
-import os, pickle, json, nltk
+import os, inspect, pickle, json, nltk
 from nltk.util import bigrams
 
 DIR = os.path.dirname(os.path.abspath(__file__))
 ROOT_DIR = DIR + '/../'
 
 
-# get new, freshly trained instance of UnigramTagger.
+def extract(sentence, stations_tagger=None, stations_list=None):
+    """
+    it merge 2 strategies:
+    1. key-word based search (using predefined list of stations)
+    2. free-text search (using UnigramTagger - see categorize_stations() )
+    :param sentence: string where we want to find stations
+    :return: list of tuples:
+     [
+        ("from", ""),
+        ("unknown station", "STATION"),
+        ("to", ""),
+        ("horsham", "STATION"),
+     ],
+    """
+    stations_tagger = load_trained_tagger() if stations_tagger is None else stations_tagger
+    stations_list = load_stations_list() if stations_list is None else stations_list
+
+    keywords_result = tag_stations_using_keywords(sentence, stations_list)
+    tagging_result = categorize_stations(sentence, stations_tagger)
+
+    return merge_results(keywords_result, tagging_result)
+
+def merge_results(result1, result2):
+    """
+
+    :param result1:
+    [
+        ('i', ''), ('want', ''), ('to', ''), ('travel', ''), ('from', ''),
+        ('manchester', ''), ('airport', ''), ('to', ''), ('zgierz', 'STATION')
+    ]
+    :param result2:
+    [
+        ('i', ''), ('want', ''), ('to', ''), ('travel', ''), ('from', ''),
+        ('manchester airport', 'STATION'), ('to', ''), ('zgierz', '')
+    ]
+    :return:
+    [
+        ('i', ''), ('want', ''), ('to', ''), ('travel', ''), ('from', ''),
+        ('manchester airport', 'STATION'), ('to', ''), ('zgierz', 'STATION')
+    ]
+    """
+    pass
+
+
+def load_stations_list():
+    file_path = os.path.dirname(
+        os.path.abspath(inspect.getfile(inspect.currentframe()))
+    ) + '/assets/stations_list.txt'
+    with open(file_path) as f:
+        stations_list = [line.strip().lower() for line in f if len(line.strip()) > 0]
+
+    return stations_list
+
+
 def train_tagger():
+    """
+    Get new, freshly trained instance of UnigramTagger.
+    :return:
+    """
     train_data_input = json.load(open(ROOT_DIR + '/src/assets/training_Sets/stationsExtractionTrainingSet.json'))
     train_data = [[(element["pos"], element["classification"]) for element in sentence] for sentence in
                   train_data_input]
@@ -16,12 +73,22 @@ def train_tagger():
     return tagger
 
 
-# load already trained classifier
 def load_trained_tagger():
+    """
+    load already trained classifier
+    :return:
+    """
     return pickle.load(open(ROOT_DIR + "/src/assets/trained_classifiers/stations_classifier.p", "rb"))
 
 
 def categorize_stations(text, stations_tagger: nltk.UnigramTagger = None):
+    """
+    Categorize stations in a "smart way" - use it to find stations which are not known.
+
+    :param text: simple string
+    :param stations_tagger: nltk.UnigramTagger - use load_trained_tagger() to get trained one
+    :return:
+    """
     stations_tagger = load_trained_tagger() if stations_tagger is None else stations_tagger
 
     tagged_sentence = nltk.pos_tag(nltk.word_tokenize(text))
@@ -30,28 +97,74 @@ def categorize_stations(text, stations_tagger: nltk.UnigramTagger = None):
         lambda tagged_word: (tagged_word[0][0], tagged_word[1][1]),  # extract (<word>, <category>)
         list(zip(tagged_sentence, tagged_pos))  # merge (<word>, <pos>) with (<pos>, <category>)
     ))
-    return _extract_stations_list_from_tagged_sentence(tagging_result)
+    return _tag_stations_full_names(tagging_result)
 
 
-# I'm too lame in functional programming to do it in functional way :)
-def _extract_stations_list_from_tagged_sentence(tagged_sentence):
+def _tag_stations_full_names(tagged_sentence):
+    """
+    I'm too lame in functional programming to do it in functional way :)
+    Probably Knuth-Smith-Pratt algorithm could be used here, but first I implemented that,
+    and then I discovered KSP algorithm :D
+
+    :param tagged_sentence:
+    [
+        ('Plan', 'O'), ('me', 'O'), ('a', 'O'), ('journey', 'O'), ('from', 'O'),
+        ('Manchester', 'STATION'), ('Airport', 'STATION'), ('to', 'O'), ('London', 'STATION')
+    ]
+    :return:
+    [
+        ('Plan', ''), ('a', ''), ('journey', ''), ('from', ''),
+        ('Manchester Airport', 'STATION'), ('to', ''), ('London', 'STATION')
+    ]
+    """
     result = []
     is_first_element = True
 
     for (word1, class1), (word2, class2) in list(bigrams(tagged_sentence)):
+        # special handling for 1st element of sequence
         if is_first_element:
             is_first_element = False
             if class1 != 'STATION':
-                continue
-            result.append(word1)
+                result.append((word1.lower(), ''))
+            else:
+                result.append((word1.lower(), 'STATION'))
 
+        # regular logic for all other elements
         if class2 != 'STATION':
+            result.append((word2.lower(), ''))
             continue
 
         if class1 == 'STATION':
             last_station = result.pop()
-            result.append(last_station + ' ' + word2)
+            result.append((last_station[0] + ' ' + word2.lower(), 'STATION'))
         else:
-            result.append(word2)
+            result.append((word2.lower(), 'STATION'))
 
     return result
+
+
+def tag_stations_using_keywords(sentence, stations_list):
+    """
+    :param sentence: string where we want to find stations
+    :param stations_list: list of stations, all should be lower case.
+    :return: list of tuples:
+     [
+        ("from", ""),
+        ("london euston", "STATION"),
+        ("to", ""),
+        ("horsham", "STATION"),
+     ],
+    """
+
+    sentence = sentence.lower()
+    implode_marker = '____'
+    stations_matched = []
+    for station in stations_list:
+        if station in sentence:
+            station_tokenized = implode_marker.join(station.split(" "))
+            sentence = sentence.replace(station, station_tokenized)
+            stations_matched.append(station_tokenized)
+    return [
+        (token.replace(implode_marker, " "), "STATION" if token in stations_matched else "")
+        for token in nltk.tokenize.word_tokenize(sentence)
+    ]
--- tests/stations_extraction_test.py
+++ tests/stations_extraction_test.py
@@ -1,7 +1,34 @@
 import src.stations_extraction
+import unittest
 
-def test_categorize_stations():
-    test_sentence = "Plan me a journey from Manchester Airport to London"
-    result = src.stations_extraction.categorize_stations(test_sentence)
 
-    assert result == ["Manchester Airport", "London"]
+class StationsExtractionTest(unittest.TestCase):
+
+    # def test_categorize_stations(self):
+    #     test_sentence = "Plan me a journey from Manchester Airport to London"
+    #     result = src.stations_extraction.categorize_stations(test_sentence)
+    #
+    #     expected = [("plan", ""), ("me", ""), ("a", ""), ("journey", ""), ("from", ""),
+    #                 ("manchester airport", "STATION"), ("to", ""), ("london", "STATION")]
+    #
+    #     self.assertSequenceEqual(seq1=result, seq2=expected)
+
+    def test_extract(self):
+        test_sentence = "I want to travel from Manchester Airport to Zgierz"
+        stations_list = ["zgierz"]
+        trained_tagger = src.stations_extraction.load_trained_tagger()
+
+        result = src.stations_extraction.extract(test_sentence, trained_tagger, stations_list)
+
+        expected = [
+            ("I", ""),
+            ("want", ""),
+            ("to", ""),
+            ("travel", ""),
+            ("from", ""),
+            ("manchester airport", "STATION"),
+            ("to", ""),
+            ("zgierz", "STATION"),
+        ]
+
+        self.assertSequenceEqual(expected, result)
